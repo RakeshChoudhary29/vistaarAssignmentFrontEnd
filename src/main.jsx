36f4499b74618b4f719d60789a30b05803ef3636
RakeshChoudhary29 <rakeshtheone29@gmail.com>
2025-05-02 12:36:20 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId={`${clientId}`}>
 
     <App />
-    /</GoogleOAuthProvider>
+    </GoogleOAuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
